c64ff5ae888e27655e776c90ac2b35281358e092
amber <novaturient@163.com>
2024-10-16 13:48:56 +0800

    package

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/fashion/',
+  base: './',
   plugins: [
     vue(),
     AutoImport({
